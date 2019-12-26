263a7b9c4914e3ede27b9b756810b5322a9cab72
Aryan Ganotra <aryanganotra7@gmail.com>
2019-12-26 12:43:03 +0530

    Created auth layout

--- src/screens/phoneauth/auth/Auth.js
+++ src/screens/phoneauth/auth/Auth.js
@@ -1,167 +1,47 @@
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-  SafeAreaView,
-  TouchableOpacity,
-  View,
-  Text,
-  TextInput
-} from 'react-native'
+import React, {useState} from 'react';
+import {View,Text, TextInput, Button, Alert, Image, Dimensions} from 'react-native';
 import auth from '@react-native-firebase/auth';
 
-class PhoneAuthScreen extends Component {
-  state = {
-    phone: '',
-    confirmResult: null,
-    verificationCode: '',
-    userId: ''
-  }
-  validatePhoneNumber = () => {
-    var regexp = /^\+[0-9]?()[0-9](\s|\S)(\d[0-9]{8,16})$/
-    return regexp.test(this.state.phone)
-  }
-
-  handleSendCode = () => {
-    // Request to send OTP
-    if (this.validatePhoneNumber()) {
-        auth()
-        .signInWithPhoneNumber(this.state.phone)
-        .then(confirmResult => {
-          this.setState({ confirmResult })
-        })
-        .catch(error => {
-          alert(error.message)
-
-          console.log(error)
-        })
-    } else {
-      alert('Invalid Phone Number')
-    }
-  }
-
-  changePhoneNumber = () => {
-    this.setState({ confirmResult: null, verificationCode: '' })
-  }
-
-  handleVerifyCode = () => {
-    // Request for OTP verification
-    const { confirmResult, verificationCode } = this.state
-    if (verificationCode.length == 6) {
-      confirmResult
-        .confirm(verificationCode)
-        .then(user => {
-          this.setState({ userId: user.uid })
-          alert(`Verified! ${user.uid}`)
-        })
-        .catch(error => {
-          alert(error.message)
-          console.log(error)
-        })
-    } else {
-      alert('Please enter a 6 digit OTP code.')
-    }
-  }
-
-  renderConfirmationCodeView = () => {
-    return (
-      <View style={styles.verificationView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Verification code'
-          placeholderTextColor='#eee'
-          value={this.state.verificationCode}
-          keyboardType='numeric'
-          onChangeText={verificationCode => {
-            this.setState({ verificationCode })
-          }}
-          maxLength={6}
-        />
-        <TouchableOpacity
-          style={[styles.themeButton, { marginTop: 20 }]}
-          onPress={this.handleVerifyCode}>
-          <Text style={styles.themeButtonTitle}>Verify Code</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
-
-  render() {
-    return (
-      <SafeAreaView style={[styles.container, { backgroundColor: '#333' }]}>
-        <View style={styles.page}>
-          <TextInput
-            style={styles.textInput}
-            placeholder='Phone Number with country code'
-            placeholderTextColor='#eee'
-            keyboardType='phone-pad'
-            value={this.state.phone}
-            onChangeText={phone => {
-              this.setState({ phone })
+const {height, width} = Dimensions.get('window');
+const Auth = () => {
+    const defaultphn = '+919827352522'
+    const [phno, onChangePhno] = useState('');
+    return(
+        <View style = {{flex:1,padding:10, justifyContent:'flex-end'}}>
+            <View style = {{flex:3, justifyContent:'center'}}>
+            <Image 
+            source = {require('../../../../assets/images/firebase-logo.png')}
+            style = {{height:100, width:'auto'}}
+            />
+            </View>
+            <View style = {{flex:2}}>
+            <Text style = {{marginVertical : 4, fontWeight : 'bold', color:'black'}}>Phone Number</Text>
+            <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginVertical : 8}}
+            onChangeText={text => onChangePhno(text)}
+            value={phno}
+            placeholder = {defaultphn}
+            />
+            <View
+             style = {{marginVertical : 8}}>
+            <Button
+            title  = 'Generate OTP'
+            onPress = {()=> {
+                Alert.alert('Otp button pressed')
             }}
-            maxLength={15}
-            editable={this.state.confirmResult ? false : true}
-          />
+            >
+            </Button>
+            </View>
+            </View>
 
-          <TouchableOpacity
-            style={[styles.themeButton, { marginTop: 20 }]}
-            onPress={
-              this.state.confirmResult
-                ? this.changePhoneNumber
-                : this.handleSendCode
-            }>
-            <Text style={styles.themeButtonTitle}>
-              {this.state.confirmResult ? 'Change Phone Number' : 'Send Code'}
-            </Text>
-          </TouchableOpacity>
-
-          {this.state.confirmResult ? this.renderConfirmationCodeView() : null}
         </View>
-      </SafeAreaView>
     )
-  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#aaa'
-  },
-  page: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  textInput: {
-    marginTop: 20,
-    width: '90%',
-    height: 40,
-    borderColor: '#555',
-    borderWidth: 2,
-    borderRadius: 5,
-    paddingLeft: 10,
-    color: '#fff',
-    fontSize: 16
-  },
-  themeButton: {
-    width: '90%',
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#888',
-    borderColor: '#555',
-    borderWidth: 2,
-    borderRadius: 5
-  },
-  themeButtonTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff'
-  },
-  verificationView: {
-    width: '100%',
-    alignItems: 'center',
-    marginTop: 50
-  }
+Auth.navigationOptions = () => ({
+    title : 'PhoneAuth'
 })
 
-export default PhoneAuthScreen
+
+
+export default Auth;
